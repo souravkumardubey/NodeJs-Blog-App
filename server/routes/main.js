e3c099f91e6144817ea90743a9b8960d6bb0d99d
souravkumardubey <skdlp2000@gmail.com>
2023-07-26 01:36:39 +0530

    home route to get all posts and pagination added

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,12 +1,59 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../models/Post");
+/**
+ * GET/
+ * HOME
+ */
+router.get("/", async (req, res) => {
+  try {
+    const details = {
+      title: "Souav Dubey Blogs",
+      description: "My personal blogging website!",
+    };
+    const postsPerPage = 5;
+    let page = req.query.page || 1;
 
-router.get("/", (req, res) => {
-  const details = {
-    title: "Souav Dubey Blogs",
-    description: "My personal blogging website!",
-  };
-  res.render("index", { details });
+    const blogs = await Post.aggregate([{ $sort: { createdOn: -1 } }])
+      .skip(page * postsPerPage - postsPerPage)
+      .limit(postsPerPage)
+      .exec();
+
+    const count = await Post.count();
+    const nextPage = +page + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / postsPerPage);
+    res.render("index", {
+      details,
+      blogs,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
+
+// router.get("/", async (req, res) => {
+//   try {
+//     const details = {
+//       title: "Souav Dubey Blogs",
+//       description: "My personal blogging website!",
+//     };
+//     const blogs = await Post.find();
+//     res.render("index", { details, blogs });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+// const insertToDB = () => {
+//   Post.insertMany([
+//     {
+//       title: "Blog 2",
+//       body: "My second blog!!",
+//     },
+//   ]);
+// };
+// insertToDB();
 
 module.exports = router;
