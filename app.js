c8c252fab2c5337a8713acd8ff2e4082564054a2
souravkumardubey <skdlp2000@gmail.com>
2023-07-26 00:24:47 +0530

    basic routes setuo

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
+
+const connectDB = require("./server/config/db");
 const app = express();
 app.use(express.static("public"));
 
 const PORT = 5000 || process.env.PORT;
+
+connectDB();
 app.use(expressLayout);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
