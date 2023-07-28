08ca6d869b06a100657bbce570f8fa046366e94b
souravkumardubey <skdlp2000@gmail.com>
2023-07-28 18:55:45 +0530

    user registration route handler added

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const adminLayout = "../views/layouts/admin";
 /**
@@ -14,6 +17,46 @@ router.get("/admin", async (req, res) => {
       description: "My personal blogging website!",
     };
     res.render("admin/index", { details, layout: adminLayout });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+/**
+ * POST
+ * Admin - Login Step
+ */
+router.post("/admin", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    console.log(req.body);
+    // res.render("admin/index", { details, layout: adminLayout });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+/**
+ * POST
+ * Admin - Register
+ */
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+      const user = await User.create({
+        username,
+        password: hashedPassword,
+      });
+      res.status(200).json({ message: "User register successfully!", user });
+    } catch (err) {
+      if (err.code === 11000)
+        return res
+          .status(409)
+          .json({ message: "User with this username already exists" });
+      return res.status(409).json("Internal server error");
+    }
   } catch (err) {
     console.log(err);
   }
